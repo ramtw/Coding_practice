a46ca8c1637de01259bcaa454c56e36ae9619389
Ram Tiwari <50477195+ramtw@users.noreply.github.com>
2025-09-21 10:29:01 +0530

    Leetcode: Second_Highest_Salary

---
+++ SQL/Second_Highest_Salary.sql
@@ -0,0 +1,16 @@
+-- Method: 1
+(select distinct salary SecondHighestSalary
+from Employee
+order by 1 desc
+limit 1 offset 1)
+union all
+select null as SecondHighestSalary
+limit 1;
+
+-- Method: 2
+--scalar sub query: return single value, handles null
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
