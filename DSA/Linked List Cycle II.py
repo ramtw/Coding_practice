6902be966bd0027768f42b650c28489d32c7aa84
Ram Tiwari <50477195+ramtw@users.noreply.github.com>
2025-09-24 21:02:33 +0530

    Create Linked List Cycle II.py

---
+++ DSA/Linked List Cycle II.py
@@ -0,0 +1,16 @@
+# Problem Link: https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        fast=head
+
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+            if(slow==fast):
+                while(head!=slow):
+                    head=head.next
+                    slow=slow.next
+                return slow
+        return None
