64416102ef149b33ed8609890d927497b5107389
Ram Tiwari <50477195+ramtw@users.noreply.github.com>
2025-09-26 13:45:12 +0530

    Update Find Median from Data Stream.py

--- DSA/Find Median from Data Stream.py
+++ DSA/Find Median from Data Stream.py
@@ -1,5 +1,39 @@
 # Problem Link: https://leetcode.com/problems/find-median-from-data-stream/
 
+# Method-1
+class MedianFinder:
+
+    def __init__(self):
+        self.lheap=[]
+        self.rheap=[]
+
+    def addNum(self, num: int) -> None:
+        if(not self.lheap):
+            heapq.heappush(self.lheap, -1*num)
+            return
+        if(self.lheap[0]*-1 <= num):
+            heapq.heappush(self.rheap, num)
+        else:
+            heapq.heappush(self.lheap, -1*num)
+        
+        while(len(self.rheap)>len(self.lheap)):
+            x=heapq.heappop(self.rheap)
+            heapq.heappush(self.lheap, -1*x)
+        while(len(self.lheap)-len(self.rheap)>1):
+            x=heapq.heappop(self.lheap)
+            heapq.heappush(self.rheap, -1*x)
+        
+        
+    def findMedian(self) -> float:
+        # print(self.lheap)
+        # print(self.rheap)
+        s=len(self.lheap)+len(self.rheap)
+        if(s%2==1):
+            return self.lheap[0]*-1
+        return ((-1*self.lheap[0])+self.rheap[0])/2
+        
+
+# Method-2
 class MedianFinder:
 
     def __init__(self):
