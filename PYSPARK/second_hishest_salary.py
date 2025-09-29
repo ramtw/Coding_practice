96b770342a818271c752f0e159a53405d6dd320b
Ram Tiwari <50477195+ramtw@users.noreply.github.com>
2025-09-29 23:10:27 +0530

    Update second_hishest_salary.py

--- PYSPARK/second_hishest_salary.py
+++ PYSPARK/second_hishest_salary.py
@@ -1,6 +1,19 @@
 # Problem Link: https://leetcode.com/problems/second-highest-salary/description/
 
 from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, LongType
+
+data = [[1, 100], [2, 200], [3, 300], [4, 300], [5, 400], [6, 500], [7, 500], [8, 600], [9, 700]]
+data1 = [[1, 100], [2, 100], [3, 100]] // no second highest salary
+
+
+schema = StructType([
+    StructField("id", LongType(), True),  
+    StructField("salary", LongType(), True) 
+])
+
+employee_df = spark.createDataFrame(data, schema=schema)
+employee_df.show()
 
 second_highest_salary_df = (
     employee_df
